e5d907d57b4d90053e64eb76f44c4f4a0a436236
Julien Hémono <jhemono@me.com>
2015-06-14 11:14:42 +0200

    Initial Commit

---
+++ LearnLyricsUITests/LearnLyricsUITests.swift
@@ -0,0 +1,35 @@
+//
+//  LearnLyricsUITests.swift
+//  LearnLyricsUITests
+//
+//  Created by Julien Hémono on 14/06/15.
+//  Copyright © 2015 Julien Hémono. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class LearnLyricsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
