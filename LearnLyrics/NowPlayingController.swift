144e226f416d1a43bed96de876bd28eeb231db1a
Julien Hémono <jhemono@me.com>
2015-06-15 12:01:28 +0200

    Now Playing Scene with an embedded VC for displaying lyrics

---
+++ LearnLyrics/NowPlayingController.swift
@@ -0,0 +1,35 @@
+//
+//  NowPlayingController.swift
+//  LearnLyrics
+//
+//  Created by Julien Hémono on 15/06/15.
+//  Copyright © 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
